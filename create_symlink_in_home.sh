09d16f6176d9fb185171f62edd1ee9b694435b4b
jh0x4s <jh0x4s@gmail.com>
2016-07-11 22:43:14 +0900

    .vim 구성(Vundle, jellybean, Vundle 플러그인 설치) 추가

--- create_symlink_in_home.sh
+++ create_symlink_in_home.sh
@@ -2,6 +2,8 @@
 
 # variables
 DOT_BASE_DIR=~/dotfiles
+DOT_LIB_DIR=$DOT_BASE_DIR/lib
+DOT_VAR_DIR=$DOT_BASE_DIR/var
 DOT_FILES="vimrc tmux.conf"
 
 # colorful output 
@@ -23,14 +25,28 @@ function install {
         info "Creating symlink to $file in home directory."
         ln -s $DOT_BASE_DIR/dotfiles/$file ~/.$file
     done
+
+    # git submodule Vundle.vim, jellybeans.vim.
+    git submodule init
+    git submodule update
+
+    # init var/vim for ~/.vim 
+    mkdir -p $DOT_BASE_DIR/var/vim/bundle
+    ln -s $DOT_LIB_DIR/Vundle.vim $DOT_VAR_DIR/vim/bundle/Vundle.vim > /dev/null 2>&1 
+    ln -s $DOT_LIB_DIR/jellybeans.vim/colors $DOT_VAR_DIR/vim/colors > /dev/null 2>&1
+    ln -s $DOT_VAR_DIR/vim ~/.vim
+
+    vim +PluginInstall +PluginUpdate +qall
 }
 
 # remove
 function remove {
     for file in $DOT_FILES; do
-        info "Deleteing ~/$file."
-        rm ~/$file
+        info "Deleteing ~/.$file."
+        rm ~/.$file
     done
+
+    rm ~/.vim
 }
 
 # main
