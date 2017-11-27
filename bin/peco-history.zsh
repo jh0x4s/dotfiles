f9a775711b1a63679a9ddeee79a8515db5d795b7
Joonhwan <jh0x4s@gmail.com>
2017-11-27 20:31:01 +0900

    Add peco script

---
+++ bin/peco-history.zsh
@@ -0,0 +1,18 @@
+# from http://qiita.com/uchiko/items/f6b1528d7362c9310da0 by uchiko
+
+function peco-select-history() {
+    local tac
+    if which tac > /dev/null; then
+        tac="tac"
+    else
+        tac="tail -r"
+    fi
+    BUFFER=$(\history -n 1 | \
+        eval $tac | \
+        peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
+
