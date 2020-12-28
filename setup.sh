dc0c0bb26b02c830c635b8f1d6d598c4f0eff59f
Joonhwan <jh0x4s@gmail.com>
2020-12-28 22:23:35 +0900

    Remove unnecessary code

--- setup.sh
+++ setup.sh
@@ -5,26 +5,12 @@ DOT_BASE_DIR=~/dotfiles
 DOT_LIB_DIR=$DOT_BASE_DIR/lib
 DOT_VAR_DIR=$DOT_BASE_DIR/var
 DOT_BIN_DIR=$DOT_BASE_DIR/bin
-DOT_FILES="tmux.conf gitconfig gitignore_global"
+DOT_FILES="tmux.conf gitignore_global"
 
-# colorful output 
-COLOR_INFO=`tput setaf 3`
-COLOR_ERR=`tput setaf 1`
-COLOR_RESET=`tput sgr0`
-
-function info {
-    echo "${COLOR_INFO}$@${COLOR_RESET}"
-}
-
-function err {
-    echo "${COLOR_ERR}$@${COLOR_RESET}"
-}
-
-# install 
 function install {
     # create symlink
     for file in $DOT_FILES; do
-        info "Creating symlink to $file in home directory."
+        echo "Creating symlink to $file in home directory."
         ln -snf $DOT_BASE_DIR/dotfiles/$file ~/.$file
     done
 
@@ -36,17 +22,16 @@ function install {
     vim +PlugUpdate +qall
 }
 
-# remove
 function remove {
     for file in $DOT_FILES; do
-        info "Deleteing ~/.$file"
+        echo "Deleteing ~/.$file"
         rm ~/.$file
     done
 
-    info "Deleteing /usr/local/bin/tm"
+    echo "Deleteing /usr/local/bin/tm"
     rm /usr/local/bin/tm
 
-    info "Deleteing ~/.vim"
+    echo "Deleteing ~/.vim"
     test -h ~/.vimrc && rm ~/.vimrc
 }
 
